2fbeeb4a46df2a15ce8770795aac3c9a73796604
ruviro <138757113+rruviro@users.noreply.github.com>
2025-01-08 01:05:47 +0800

    Update RetrofitClient.kt

--- app/src/main/java/com/application/herbafill/Api/RetrofitClient.kt
+++ app/src/main/java/com/application/herbafill/Api/RetrofitClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "https://herbafil-api.vercel.app/"
+    private const val BASE_URL = "https://herbafil-api-mu.vercel.app/"
     val instance: ApiService by lazy {
         val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
